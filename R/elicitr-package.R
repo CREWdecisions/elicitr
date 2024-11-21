8b59dc1d1dc6f8be8528f43a773d252512cc77ff
sgvignali <22011122+sgvignali@users.noreply.github.com>
2024-11-21 09:22:49 +0100

    Clean namespace

--- R/elicitr-package.R
+++ R/elicitr-package.R
@@ -3,24 +3,12 @@
 
 ## usethis namespace: start
 #' @importFrom cli cli_abort
-#' @importFrom cli cli_h3
+#' @importFrom cli cli_h2
 #' @importFrom cli cli_li
 #' @importFrom cli cli_text
 #' @importFrom cli col_magenta
 #' @importFrom cli style_underline
-#' @importFrom digest digest
-#' @importFrom dplyr arrange
-#' @importFrom dplyr mutate
-#' @importFrom dplyr row_number
-#' @importFrom dplyr select
-#' @importFrom googlesheets4 read_sheet
-#' @importFrom openxlsx read.xlsx
-#' @importFrom rlang .data
 #' @importFrom rlang caller_env
-#' @importFrom stats setNames
 #' @importFrom stringr str_split_1
-#' @importFrom tibble as_tibble
-#' @importFrom tools file_ext
-#' @importFrom utils read.csv
 ## usethis namespace: end
 NULL
