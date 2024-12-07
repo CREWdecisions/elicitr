118a41c3b402885e16722d8890be77b1631328fd
Sergio Vignali <22011122+sgvignali@users.noreply.github.com>
2024-12-07 12:28:09 +0100

    Rename files (#60)
    * Rename R files
    
    * Rename test files
    
    * Rebuid documentation

--- R/elic_cont.R
+++ R/elic_cont.R
@@ -1,3 +1,29 @@
+#' elic_cont class
+#'
+#' The `elic_cont` class is a container for the elicitation data. It is used to
+#' store the data collected during the elicitation process and their matadata.
+#'
+#' @section Object elements:
+#'
+#' There are 5 elements in the `elic_cont` object:
+#'
+#' * `var_names`: character vector with the name of the estimated variables.
+#' * `var_types`: character string with short codes indicating the variable
+#' type.
+#' * `elic_types`: character string with short codes indicating the elicitation
+#' type.
+#' * `experts`: numeric indicating the number of experts participating at the
+#' elicitation process.
+#' * `data`: list with the data collected during the elicitation process. The
+#' list has two elements, `round_1` and `round_2`, which store the data
+#' collected during the first and second round of the elicitation process,
+#' respectively.
+#'
+#' Moreover, the object has a `title` attribute that binds a name to the object.
+#'
+#' @name elic_cont
+NULL
+
 # Constructor for the `elic_cont` object
 new_elic_cont <- function(var_names,
                           var_types,
