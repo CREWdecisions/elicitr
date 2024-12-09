c558d3b0aba8de6866615d4b077afcd779f48d2a
sgvignali <22011122+sgvignali@users.noreply.github.com>
2024-12-09 14:38:24 +0100

    Improve dataset

--- data-raw/exported_cat_data.R
+++ data-raw/exported_cat_data.R
@@ -15,46 +15,44 @@ n_sites <- length(sites)
 
 # Generate random values for each level that sum up to 100
 get_values <- function() {
-  Surrogate::RandVec(a = 0, b = 100, s = 100,
-                     n = n_levels, m = n_experts * n_sites)[[1]] |>
+  rand_data <- Surrogate::RandVec(a = 0, b = 1, s = 1,
+                                  n = n_levels, m = n_experts * n_sites)[[1]] |>
+    round(2)
+
+  diff <- colSums(rand_data) - 1
+
+  # Correct last elements to sum up to 1
+  rand_data[n_levels, ] <- rand_data[n_levels, ] - diff
+
+  rand_data |>
     as.vector()
 }
 
+mechanism <- data.frame(expert = rep(random_names, each = n_levels * n_sites),
+                        level = rep(levels, times = n_experts * n_sites),
+                        site = rep(rep(sites, each = n_levels),
+                                   times = n_experts),
+                        confidence = rep(sample(seq(0, 100, by = 5),
+                                                size = n_experts * n_sites,
+                                                replace = TRUE),
+                                         each = n_levels),
+                        estimate = get_values()) |>
+  tibble::as_tibble()
 
-mechanism_1 <- data.frame(expert = rep(random_names, each = n_levels * n_sites),
-                          level = rep(levels, times = n_experts * n_sites),
-                          site = rep(rep(sites, each = n_levels),
-                                     times = n_experts),
-                          uncertatnty = rep(sample(1:100, n_experts * n_sites),
-                                            each = n_levels),
-                          value = get_values())
-
-mechanism_2 <- data.frame(expert = rep(random_names, each = n_levels * n_sites),
-                          level = rep(levels, times = n_experts * n_sites),
-                          site = rep(rep(sites, each = n_levels),
-                                     times = n_experts),
-                          uncertatnty = rep(sample(1:100, n_experts * n_sites),
-                                            each = n_levels),
-                          value = get_values())
-
-usethis::use_data(mechanism_1, mechanism_2,
+usethis::use_data(mechanism,
                   internal = FALSE,
                   overwrite = TRUE,
                   version = 3)
 
 # Save datasets into extdata folder
 # 2 csv files, one for each dataset
-write.csv(mechanism_1,
-          file = file.path("inst", "extdata", "mechanism_1.csv"),
-          row.names = FALSE)
-write.csv(mechanism_2,
-          file = file.path("inst", "extdata", "mechanism_2.csv"),
+write.csv(mechanism,
+          file = file.path("inst", "extdata", "mechanism.csv"),
           row.names = FALSE)
 
 # 1 xlsx file with two sheets
-openxlsx::write.xlsx(list("Mechanism 1" = mechanism_1,
-                          "Mechanism 2" = mechanism_2),
-                     file = file.path("inst", "extdata", "mechanisms.xlsx"),
+openxlsx::write.xlsx(list("Mechanism" = mechanism),
+                     file = file.path("inst", "extdata", "mechanism.xlsx"),
                      overwrite = TRUE)
 
 # Save dataset in Google Sheets
@@ -64,12 +62,9 @@ openxlsx::write.xlsx(list("Mechanism 1" = mechanism_1,
 # gs <- googlesheets4::gs4_create("mechanisms",
 #                                 sheets = c("Mechanism 1", "Mechanism 2"))
 gs <- "18VHeHB89P1s-6banaVoqOP-ggFmQZYx-z_31nMffAb8"
-googlesheets4::sheet_write(mechanism_1,
+googlesheets4::sheet_write(mechanism,
                            ss = gs,
                            sheet = 1)
-googlesheets4::sheet_write(mechanism_2,
-                           ss = gs,
-                           sheet = 2)
 
 # !IMPORTANT! Go to Google Drive and change the Time column format to Date Time
 # Once the files are created, go to Google Drive and make them public for view
