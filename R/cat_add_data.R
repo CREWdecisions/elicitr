b58fa50c2b1a6c8500301bd38ef2a6202f3b4753
sgvignali <22011122+sgvignali@users.noreply.github.com>
2024-12-10 13:51:20 +0100

    Use link to function

--- R/cat_add_data.R
+++ R/cat_add_data.R
@@ -3,14 +3,14 @@
 #' @description
 #' `r lifecycle::badge("experimental")`
 #'
-#' `elic_cat_add_data()` adds data to an `elic_cat` object from different
+#' `elic_cat_add_data()` adds data to an [elic_cat] object from different
 #' sources.
 #'
-#' @param x
+#' @param x an object of class [elic_cat].
 #' @param mechanism
 #' @inheritParams elic_cont_add_data
 #'
-#' @return The provided object of class `elic_cat` updated with the data.
+#' @return The provided object of class [elic_cat] updated with the data.
 #' @export
 #'
 #' @examples
