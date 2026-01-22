e1ff2b9c92120992eb27cb8280f48138e7a13696
maude-v <maude.vernet@students.unibe.ch>
2026-01-22 11:23:13 +0100

    Update lint (3)

--- R/plot_cat_sample.R
+++ R/plot_cat_sample.R
@@ -142,13 +142,13 @@ plot.cat_sample <- function(x,
                            key_glyph = "dotplot")
   } else if (type == "beeswarm") {
     p <- ggplot2::ggplot(x) +
-      ggbeeswarm::geom_beeswarm(
-        mapping = ggplot2::aes(x = .data[["category"]],
-                               y = .data[["prob"]],
-                               colour = .data[["category"]]),
-        cex = beeswarm_cex,
-        size = 1,
-        corral = beeswarm_corral)
+      ggbeeswarm::geom_beeswarm(mapping = ggplot2::aes(x = .data[["category"]],
+                                                       y = .data[["prob"]],
+                                                       colour =
+                                                         .data[["category"]]),
+                                cex = beeswarm_cex,
+                                size = 1,
+                                corral = beeswarm_corral)
   } else {
 
     info <- "Available types are {.val beeswarm} and {.val violin}."
